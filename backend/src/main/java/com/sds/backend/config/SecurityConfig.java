a205b9d56181151d01a3fcfd25a632e0938c3e2b
Shubham Satpute <shubhamdsatpute77@gmail.com>
2026-03-19 15:49:41 +0530

    login api added

--- backend/src/main/java/com/sds/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/sds/backend/config/SecurityConfig.java
@@ -2,9 +2,10 @@ package com.sds.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,12 +23,16 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
                 .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- backend/src/main/java/com/sds/backend/controller/AuthController.java
+++ backend/src/main/java/com/sds/backend/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.sds.backend.controller;
 
 import com.sds.backend.common.ApiResponseMeta;
+import com.sds.backend.dto.LoginRequest;
 import com.sds.backend.dto.RegisterUserRequest;
 import com.sds.backend.dto.UserResponse;
 import com.sds.backend.service.UserService;
@@ -8,6 +9,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +23,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("register")
     @ApiResponseMeta(message = "User registered successfully")
     public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterUserRequest registerUserRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(registerUserRequest));
+    }
+
+    @PostMapping("login")
+    public String login(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.email(),
+                        loginRequest.password()
+                )
+        );
+        return "Login Successfully";
     }
 }
